bb728240fcd8e1766d8c2d4953b06637c67bfc14
TomRiddle08 <misha.simfi@gmail.com>
2025-06-15 18:46:37 +0300

    test fix2

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -6,14 +6,22 @@ class TestBasic : public QObject
 {
     Q_OBJECT
 private slots:
-    void testEventCreation() {
-        Event e("2025-06-15", "Vaccine", "Rabies");
-        QCOMPARE(e.getType(), QString("Vaccine"));
+    void testEventCreation()
+    {
+        QDateTime dt = QDateTime::currentDateTime();
+        Event e("Барсик", "Ежегодная прививка", dt, Event::Type::Vaccination);
+        
+        QCOMPARE(e.getPetName(), QString("Барсик"));
+        QCOMPARE(e.getType(), Event::Type::Vaccination); // Сравниваем enum
+        QCOMPARE(e.getTypeString(), QString("Прививка")); // Сравниваем строки
     }
     
-    void testPetCreation() {
-        Pet p("Fluffy", "Cat", 2);
-        QCOMPARE(p.getSpecies(), QString("Cat"));
+    void testPetCreation()
+    {
+        Pet p("Мурзик", "Кот", "Домашний", 3, 4.5, "Рыжий", "Пушистый");
+        
+        QCOMPARE(p.getName(), QString("Мурзик"));
+        QCOMPARE(p.getBreed(), QString("Домашний")); // Используем существующий метод
     }
 };
 
